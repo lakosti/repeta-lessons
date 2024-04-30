097d675894ff8f2b0e7672cc264b216848966c28
Yaroslava <yaroslava.hovenko@gmail.com>
2024-04-30 22:54:09 +0300

    add dispatch action

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit } from "../redux/store";
 
 export default function Balance() {
   const balance = useSelector((state) => state.balance.value);
+  const dispatch = useDispatch();
 
   return (
     <div>
       <p>Balance: {balance}</p>
-      <button> Deposit credits</button>
+      <button onClick={() => dispatch(deposit(5))}> Deposit credits</button>
       <button> Withdraw credits</button>
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,19 @@
 import { createStore } from "redux";
 
+export const deposit = (value) => {
+  return {
+    type: "balance/deposit",
+    payload: value,
+  };
+};
+
 const initialState = {
   balance: {
     value: 0,
   },
 };
 const rootReducer = (state = initialState, action) => {
+  console.log(action);
   return state;
 };
 export const store = createStore(rootReducer);
